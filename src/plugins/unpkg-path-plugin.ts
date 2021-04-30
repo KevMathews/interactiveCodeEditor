6252187845cd17a6404572002ddc1406f0d5645f
Kevin Mathews <kev.math@yahoo.com>
2021-04-30 15:37:32 -0700

    changed the filtering and handling of files

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,28 +1,31 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
-import localforage from 'localforage';
+import localForage from 'localforage';
 
-const fileCache = localforage.createInstance({
-
+const fileCache = localForage.createInstance({
+  name: 'filecache',
 });
 
-
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
+      // added this to to handle root entry file of 'index.js'
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: 'index.js', namespace: 'a' };
+      });
+
+      // added this to handle relative paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: 'a',
+          path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/')
+            .href,
+        };
+      });
+
+      // this handles main file of a module
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' };
-        }
-
-        if (args.path.includes('./') || args.path.includes('../')) {
-          return {
-            namespace: 'a',
-            path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href,
-          };
-        }
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`,
@@ -30,18 +33,19 @@ export const unpkgPathPlugin = () => {
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log('onLoad', args);
 
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              const message = require('react-dom');
-              console.log(message);
-            `,
+            contents: inputCode,
           };
         }
 
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
         if (cachedResult) {
           return cachedResult;
         }
@@ -50,15 +54,12 @@ export const unpkgPathPlugin = () => {
         const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
-          resolveDir: new URL('./', request.responseURL).pathname
+          resolveDir: new URL('./', request.responseURL).pathname,
         };
         await fileCache.setItem(args.path, result);
+
         return result;
       });
     },
   };
 };
-
-
-
-
