8baa80fe713f8bb0770ac4e737199ecde7e876fd
Kevin Mathews <kev.math@yahoo.com>
2021-04-30 13:22:41 -0700

    fixed ts error with the cache

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -40,25 +40,19 @@ export const unpkgPathPlugin = () => {
             `,
           };
         }
-        //  Here I check to see if I have already fetched this file
-        //  and if it is in th cache
-        const cachedResult = await fileCache.getItem(args.path);
 
-        //  if it is, return it immediately
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
         if (cachedResult) {
           return cachedResult;
         }
         const { data, request } = await axios.get(args.path);
 
-        const result = {
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname
         };
-
-        //  store response in cache
         await fileCache.setItem(args.path, result);
-
         return result;
       });
     },
