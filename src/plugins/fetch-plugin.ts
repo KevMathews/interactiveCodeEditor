1c00391cdb259402ba9c943c1457c7f632d71bf3
Kevin Mathews <kev.math@yahoo.com>
2021-05-01 17:22:31 -0700

    trying work around for importing css

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -6,14 +6,11 @@ const fileCache = localForage.createInstance({
   name: 'filecache',
 });
 
-
 export const fetchPlugin = (inputCode: string) => {
   return {
     name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
-
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
@@ -30,17 +27,31 @@ export const fetchPlugin = (inputCode: string) => {
         // }
         const { data, request } = await axios.get(args.path);
 
-        const loader = args.path.match(/.css$/) ? 'css' : 'jsx';
+        const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
+
+        const escaped = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+
+        const contents =
+          fileType === 'css'
+            ? `
+            const style = document.createElement('style');
+            style.innerText = '${escaped}';
+            document.head.appendChild(style);
+          `
+            : data;
 
         const result: esbuild.OnLoadResult = {
-          loader,
-          contents: data,
+          loader: 'jsx',
+          contents,
           resolveDir: new URL('./', request.responseURL).pathname,
         };
         await fileCache.setItem(args.path, result);
 
         return result;
       });
-    }
-  }
-}
+    },
+  };
+};
