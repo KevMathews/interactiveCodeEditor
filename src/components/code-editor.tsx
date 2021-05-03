46b6d3fc39b47a5ea867b24efcbc7edfbaa0e5b6
Kevin Mathews <kev.math@yahoo.com>
2021-05-03 12:37:53 -0700

    trying to bundle code from the editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,38 @@
 import MonacoEditor from '@monaco-editor/react';
 
-const CodeEditor = () => {
-  return <MonacoEditor language="javascript" theme="dark" height="500px" />;
+interface CodeEditorProps {
+  initialValue: string;
+  onChange(value: string): void;
+}
+const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
+  const onEditorDidMount = (getValue: () => string, monacoEditor: any) => {
+
+    monacoEditor.onDidChangeModelContent(() => {
+      // console.log(getValue());
+      onChange(getValue());
+    })
+  };
+
+  return (
+    <MonacoEditor
+      editorDidMount={onEditorDidMount}
+      // value is used to set initial editor text/values
+      value={initialValue}
+      theme="dark"
+      language="javascript"
+      height="500px"
+      options={{
+        wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 18,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+
+      }}
+    />);
 };
 
 export default CodeEditor;
