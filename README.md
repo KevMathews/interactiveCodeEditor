ff354067f9e08c8e8c94522fab869291164d72a9
Kevin Mathews <kev.math@yahoo.com>
2021-05-04 16:02:37 -0700

    working on making code editor windows resizable using react-resizable

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# interactiveCodeEditor
+# Interactive Code Editor
+
+## building this feature to add to API-Go
+
+## [API-Go](https://api-go.app)
+
+<hr />
+
+### [KevMathews.com](https://www.kevmathews.com)
+
+
--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
 import bundle from '../bundler';
-
+import Resizable from './resizable';
 const CodeCell = () => {
   const [code, setCode] = useState('');
   const [input, setInput] = useState('');
@@ -13,18 +13,17 @@ const CodeCell = () => {
   };
 
   return (
-    <div>
-      {/* initial value of editor is passed below */}
-      <CodeEditor
-        initialValue="const a = 1;
+    <Resizable direction='vertical'>
+      <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
+        {/* initial value of editor is passed below */}
+        <CodeEditor
+          initialValue="const a = 1;
                       console.log(a);"
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
+          onChange={(value) => setInput(value)}
+        />
+        <Preview code={code} />
       </div>
-      <Preview code={code} />
-    </div>
+    </Resizable>
   );
 };
 
