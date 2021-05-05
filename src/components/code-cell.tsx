80f6461d8ee7973c014e8f0ce44353c9c9e9ae2e
Kevin Mathews <kev.math@yahoo.com>
2021-05-04 20:02:23 -0700

    working on how it handles errors in browser

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -6,12 +6,15 @@ import Resizable from './resizable';
 
 const CodeCell = () => {
   const [code, setCode] = useState('');
+  const [err, setErr] = useState('');
   const [input, setInput] = useState('');
 
   useEffect(() => {
     const timer = setTimeout(async () => {
       const output = await bundle(input);
-      setCode(output);
+      // setCode(output);
+      setCode(output.code);
+      setErr(output.err);
     }, 750);
 
     return () => {
@@ -35,7 +38,7 @@ const CodeCell = () => {
             onChange={(value) => setInput(value)}
           />
         </Resizable>
-        <Preview code={code} />
+        <Preview code={code} err={err} />
       </div>
     </Resizable>
   );
