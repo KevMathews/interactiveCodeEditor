1e5b6bf4d7f78db88262174391d8bef7b510fdb7
Kevin Mathews <kev.math@yahoo.com>
2021-05-04 18:44:21 -0700

    editor windows resizable, working on debuggin a few errors

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,3 +1,4 @@
+import './preview.css';
 import { useRef, useEffect } from 'react';
 
 interface PreviewProps {
@@ -6,7 +7,9 @@ interface PreviewProps {
 
 const html = `
     <html>
-      <head></head>
+      <head>
+        <style>html { background-color: white; }</style>
+      </head>
       <body>
         <div id="root"></div>
         <script>
@@ -29,17 +32,20 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
 
   useEffect(() => {
     iframe.current.srcdoc = html;
-    iframe.current.contentWindow.postMessage(code, '*');
+    setTimeout(() => {
+      iframe.current.contentWindow.postMessage(code, '*');
+    }, 50);
   }, [code]);
 
   return (
-    <iframe
-      style={{ backgroundColor: 'white' }}
-      title="preview"
-      ref={iframe}
-      sandbox="allow-scripts"
-      srcDoc={html}
-    />
+    <div className="preview-wrapper">
+      <iframe
+        title="preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
+    </div>
   );
 };
 
