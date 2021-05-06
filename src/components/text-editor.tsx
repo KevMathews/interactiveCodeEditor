e3f2082294fdd08b3bacf002c7f69518c7e39b14
Kevin Mathews <kev.math@yahoo.com>
2021-05-06 13:28:52 -0700

    working of markdown editor toggle mode and tryign to troubleshoot name conflicts

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,10 +1,41 @@
+import { useState, useEffect, useRef } from 'react';
 import MDEditor from '@uiw/react-md-editor';
 
 const TextEditor: React.FC = () => {
+  const [editing, setEditing] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (ref.current && event.target && ref.current.contains(event.target as Node)) {
+        console.log('this click is inside editor');
+        return;
+      }
+      console.log('this click is not inside editor')
+      setEditing(false);
+      // console.log(event.target)
+    };
+    //  console.log(listener)
+    document.addEventListener('click', listener, { capture: true });
+
+    return () => {
+      document.removeEventListener('click', listener, { capture: true })
+    };
+
+  }, [])
+
+  if (editing) {
+    return (
+      <div ref={ref}>
+        <MDEditor />
+      </div>
+    )
+  }
   return (
-    <div>
-      <MDEditor />
-      {/* <MDEditor.Markdown source={'# Header'} /> */}
+    <div onClick={() => {
+      setEditing(true);
+    }}>
+      {/* <MDEditor /> */}
+      <MDEditor.Markdown source={'# Header'} />
     </div>
 
   )
