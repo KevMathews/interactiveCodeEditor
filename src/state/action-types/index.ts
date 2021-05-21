1a3dceef740f33c664dcd3995bf49c30036d12d3
Kevin Mathews <kev.math@yahoo.com>
2021-05-21 13:11:06 -0700

    created redux store

--- src/state/action-types/index.ts
+++ src/state/action-types/index.ts
@@ -2,7 +2,5 @@ export enum ActionType {
   MOVE_CELL = 'move_cell',
   DELETE_CELL = 'delete_cell',
   INSERT_CELL_BEFORE = 'insert_cell_before',
-  // Not sure if i will allow inserting of a cell after or just before
-  //  INSERT_CELL_AFTER = 'insert_cell_after',
   UPDATE_CELL = 'update_cell',
 }
